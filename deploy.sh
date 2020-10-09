1a34e7ac64fffd7ccb66153328b0c35336c966eb
Filbar <music@filbar.xyz>
2020-10-09 09:22:11 -0700

    Updating CI/Deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,14 @@ cd public
 
 if [ -n "$GITHUB_AUTH_SECRET" ]
 then
-    echo -e "Creating credentials"
     touch ~/.git-credentials
     chmod 0600 ~/.git-credentials
-    echo $GITHUB_AUTH_SECRET > ~/.git-credentials    git config credential.helper store
+    echo $GITHUB_AUTH_SECRET > ~/.git-credentials
+    git config credential.helper store
     git config user.email "filbar-music-bot@users.noreply.github.com"
     git config user.name "filbar-music-bot"
 fi
 
-echo -e "Git add..."
 git add .
-echo -e "Git commit..."
 git commit -m "Rebuild site"
-echo -e "Git push..."
 git push --force origin HEAD:main
-echo -e "Git done..."
