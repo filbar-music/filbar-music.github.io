473d260876d23bb56294d794611bf15f01200eb1
Filbar <music@filbar.xyz>
2020-10-09 08:52:03 -0700

    Updating deployment script

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ cd public
 
 if [ -n "$GITHUB_AUTH_SECRET" ]
 then
+    echo -e "Creating credentials"
     touch ~/.git-credentials
     chmod 0600 ~/.git-credentials
     echo $GITHUB_AUTH_SECRET > ~/.git-credentials    git config credential.helper store
@@ -13,6 +14,10 @@ then
     git config user.name "filbar-music-bot"
 fi
 
+echo -e "Git add..."
 git add .
+echo -e "Git commit..."
 git commit -m "Rebuild site"
+echo -e "Git push..."
 git push --force origin HEAD:main
+echo -e "Git done..."
